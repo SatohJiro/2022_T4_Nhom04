bc69ce5b2bf0e1d1530aa779ce2bf57b206c63e0
Satoh Jiro <trananhq2345@gmail.com>
2022-12-22 19:19:49 +0700

    get connecting of stagging

--- src/main/java/staging/ConnectStaging.java
+++ src/main/java/staging/ConnectStaging.java
@@ -6,7 +6,7 @@ public class ConnectStaging {
     private static String userName ="root";
     private static String pass="0989702459as";
     private static String url = "jdbc:mysql://localhost:3306/stagging";
-
+// get connecting of stagging
     public static Connection getStagingConnection() throws SQLException {
                 return  DriverManager.getConnection(url, userName, pass);
     }
