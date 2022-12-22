9355166e6942fa7c05846a96a9a409489d27a107
Satoh Jiro <trananhq2345@gmail.com>
2022-12-22 19:22:58 +0700

    get connect of warehouse database

--- src/main/java/warehouse/ConnectWarehouse.java
+++ src/main/java/warehouse/ConnectWarehouse.java
@@ -8,7 +8,7 @@ public class ConnectWarehouse {
     private static String userName ="root";
     private static String pass="0989702459as";
     private static String url = "jdbc:mysql://localhost:3306/warehouse";
-
+//get connect of warehouse database
     public static Connection getWarehouseConnection() throws SQLException {
         return  DriverManager.getConnection(url, userName, pass);
     }
